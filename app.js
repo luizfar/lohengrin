883d4ac7d7617a851a4f518c957c0a563dd3231f
mozair <macskeptic@gmail.com>
2013-03-23 13:18:32 -0300

    more cleanup, use less, use lodash instead of underscore, jquery 2.0

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-var express = require('express');
-var routes = require('./routes/all');
-var http = require('http');
-var path = require('path');
-var config = require('./lib/config/all');
 /*jslint nomen: true*/
-var dirname = __dirname;
+require('./lib/lohengrin').start(__dirname);
 /*jslint nomen: false*/
-
-var app = express();
-
-app.configure(function () {
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('less-middleware')({ src: dirname + '/public' }));
-  app.use(express['static'](path.join(dirname, 'public')));
-});
-
-app.configure('development', function () {
-  app.use(express.errorHandler());
-});
-
-routes.register(app);
-
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
--- routes/config.js
+++
@@ -1,3 +0,0 @@
-exports.index = function (req, res) {
-  res.json(require('../lib/config/all').clientConfig);
-};
